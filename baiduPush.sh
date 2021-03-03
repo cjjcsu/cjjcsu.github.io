40798b014fe49db2a0c85b097b56ea11f6f49db0
蔡洁 <caijiejie@cai-inc.com>
2021-03-03 11:51:39 +0800

    :ambulance:

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://cjjcsu.com&token=8a568845212e4fe780218cdf9b1160eb"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://cjjcsu.com&token=00gLPN5VyC2EnZic"
 
 rm -rf urls.txt # 删除文件
